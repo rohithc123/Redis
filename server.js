63e4b8bd0bc38fbbe834835cd8f281f3228a3a7e
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-04 22:09:04 +0530

    Adding redis client to server file and mitigating nodemon error in package json file

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const Redis = require("redis");
+
+//pass url when using it in production
+// const redisClient = Redis.createClient({url:});
+
+const redisClient = Redis.createClient();
+redisClient.connect().catch(console.error);
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
@@ -11,6 +20,9 @@ app.get("/photos", async (req, res) => {
     `https://jsonplaceholder.typicode.com/photos`,
     { params: { albumId } }
   );
+
+  redisClient.set("photos", JSON.stringify(data), "EX", DEFAULT_EXPIRATION);
+  res.json(data);
 });
 
 app.get("/photos/:id", async (req, res) => {
@@ -20,6 +32,4 @@ app.get("/photos/:id", async (req, res) => {
   res.json(data);
 });
 
-app.listen(8000, () => {
-    
-});
+app.listen(8000, () => {});
